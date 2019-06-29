72b129c4f0e430d2d9e703987997e8bb3856592e
Elena María del Río <43640427+Elenadr@users.noreply.github.com>
2019-06-29 13:23:21 +0200

    LOG Y RGISTER

--- uaclient.py
+++ uaclient.py
@@ -23,6 +23,11 @@ class Ua1Handler(ContentHandler):
     def get_tags(self):
         return self.data
 
+def log(message, log_path):
+    fich = open(log_path, "a")
+    fich.write(time.strftime('%Y%m%d%H%M%S '))
+    fich.write(menssage + "\r\n")
+    fich.close()
 
 if __name__ == "__main__":
     try:
@@ -32,10 +37,30 @@ if __name__ == "__main__":
     except IndexError:
         sys.exit("Usage: python uaclient.py config method option")
 
-    parser = make_parser() #lee linea a linea y busca etiquetas, generico para xml
-    uHandler = Ua1Handler() #Hace cosas dependiendo de la etiqueta
+    parser = make_parser()
+    uHandler = Ua1Handler()
     parser.setContentHandler(uHandler)
     parser.parse(open(CONFIG))
     data = uHandler.get_tags()
-    for diccionarios in data:
-        print(diccionarios)
+    print(data)
+
+    ADDRESS = data['account_username']
+    PORT = data['uaserver_puerto']
+    FILELOG = data['log_path']
+    IPPROXY = data['regproxy_ip']
+    PORTPROXY = data['regproxy_puerto']
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IPPROXY, PORTPROXY))
+        LINE = ''
+
+        if METHOD == 'REGISTER':
+            LINE = (METHOD + ' sip:' + ADDRESS + ':' + PORT +
+                    ' SIP/2.0\r\n' + ' Expires:' + sys.argv[3] + ' \r\n')
+            print("Enviando:", LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+            print('Recibido -- ', data.decode('utf-8'))
+
+
