ecfee20d1a97079014a58797350549c70a957f87
Elena María del Río <43640427+Elenadr@users.noreply.github.com>
2019-06-29 17:56:39 +0200

    METODOSBASICO FUNCIONANDO

--- uaclient.py
+++ uaclient.py
@@ -6,6 +6,7 @@ import time
 import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import os
 
 class Ua1Handler(ContentHandler):
     def __init__(self):
@@ -48,30 +49,58 @@ if __name__ == "__main__":
     PORT = dato['uaserver_puerto']
     LOGFILE = dato['log_path']
     IPPROXY = dato['regproxy_ip']
-    PORTPROXY = dato['regproxy_puerto']
+    PORTPROXY = int(dato['regproxy_puerto'])
     SERVER = dato['uaserver_ip']
+    AUDIOPORT = int(dato['rtpaudio_puerto'])
+    AUDIOFILE = dato['audio_path']
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         my_socket.connect((IPPROXY, int(PORTPROXY)))
-        log("Starting...", LOGFILE)
 
-        if METHOD == 'REGISTER':
 
-            LINE = (METHOD + ' sip:' + ADDRESS + ':' + PORT +
-                    ' SIP/2.0\r\n' + ' Expires:' + sys.argv[3] + ' \r\n')
-            print("Enviando:", LINE)
+        if METHOD == 'INVITE':
+
+            LINE = (METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n' +
+                     'Content-Type: application/sdp\r\n\r\n' + 'v=0\r\n' +
+                     'o=' + ADDRESS + ' ' + SERVER + '\r\n' + 's=misesion\r\n' +
+                     'm=audio ' + str(AUDIOPORT) + ' RTP' + '\r\n\r\n')
+
+
             my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            print(LINE)
             LINE = LINE.replace("\r\n", " ")
-            log('Sent to ' + SERVER + ':' + str(PORTPROXY) + ': ' + LINE, LOGFILE)
+
+
+            log('Sent to ' + SERVER + ':' + str(IPPROXY) + ': ' + LINE, LOGFILE)
             try:
                 data = my_socket.recv(1024)
-                print('Recibido -- ', data.decode('utf-8'))
             except ConnectionRefusedError:
                 log("Error: No server listening at " + IPPROXY +
                     " port " + str(PORTPROXY), LOGFILE)
-        elif METHOD == 'INVITE' :
-            
+
+        if METHOD == 'REGISTER':
+            log("Starting...", LOGFILE)
+            LINE = (METHOD + ' sip:' + ADDRESS + ':' + PORT +
+                     ' SIP/2.0\r\n' + 'Expires: ' + OPTION + '\r\n\r\n')
+            print(LINE)
+
+        if METHOD == 'ACK' :
+            LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
+            print("Enviando: \r\n" + LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+
+        if METHOD == 'BYE' :
+            LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n"
+            print(LINE)
+            log("Sent to " + SERVER + ": " + PORT + ":" + LINE, LOGFILE)
+            my_socket.send(bytes(LINE, 'utf-8'))
+
+
+
+
+
+
 
 
 
