0eefad3064fb463c6b65c5b7c32f7d08bd72f565
Elena María del Río <43640427+Elenadr@users.noreply.github.com>
2019-06-30 14:14:02 +0200

    intentando solucionar errores

--- uaclient.py
+++ uaclient.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import os
 import sys
 import time
 import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import os
+import hashlib
 
 class Ua1Handler(ContentHandler):
     def __init__(self):
@@ -23,8 +23,8 @@ class Ua1Handler(ContentHandler):
     def get_tags(self):
         return self.diccdato
 
-def log(mensaje, log_path):
-    fich = open(log_path, "a")
+def log(mensaje, LOGFILE):
+    fich = open(LOGFILE, "a")
     fich.write(time.strftime('%Y%m%d%H%M%S '))
     fich.write(mensaje + "\r\n")
     fich.close()
@@ -36,7 +36,6 @@ if __name__ == "__main__":
         OPTION = sys.argv[3]
     except IndexError:
         sys.exit("Usage: python uaclient.py config method option")
-
     parser = make_parser()
     uHandler = Ua1Handler()
     parser.setContentHandler(uHandler)
@@ -44,20 +43,22 @@ if __name__ == "__main__":
     dato = uHandler.get_tags()
     print(dato)
 
-
+    PASSWORD = dato['account_passwd']
     ADDRESS = dato['account_username']
     PORT = dato['uaserver_puerto']
     LOGFILE = dato['log_path']
     IPPROXY = dato['regproxy_ip']
-    PORTPROXY = int(dato['regproxy_puerto'])
+    PORTPROXY = dato['regproxy_puerto']
     SERVER = dato['uaserver_ip']
-    AUDIOPORT = int(dato['rtpaudio_puerto'])
+    AUDIOPORT = dato['rtpaudio_puerto']
     AUDIOFILE = dato['audio_path']
+
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         my_socket.connect((IPPROXY, int(PORTPROXY)))
-
+        log("Starting...", LOGFILE)
 
         if METHOD == 'INVITE':
 
@@ -66,42 +67,50 @@ if __name__ == "__main__":
                      'o=' + ADDRESS + ' ' + SERVER + '\r\n' + 's=misesion\r\n' +
                      'm=audio ' + str(AUDIOPORT) + ' RTP' + '\r\n\r\n')
 
-
-            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
             print(LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
             LINE = LINE.replace("\r\n", " ")
-
-
             log('Sent to ' + SERVER + ':' + str(IPPROXY) + ': ' + LINE, LOGFILE)
-            try:
-                data = my_socket.recv(1024)
-            except ConnectionRefusedError:
-                log("Error: No server listening at " + IPPROXY +
-                    " port " + str(PORTPROXY), LOGFILE)
 
         if METHOD == 'REGISTER':
             log("Starting...", LOGFILE)
             LINE = (METHOD + ' sip:' + ADDRESS + ':' + PORT +
                      ' SIP/2.0\r\n' + 'Expires: ' + OPTION + '\r\n\r\n')
             print(LINE)
-
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+            print(data)
         if METHOD == 'ACK' :
             LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
             print("Enviando: \r\n" + LINE)
-            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            aEjecutar = "./mp32rtp -i " + SERVER + " -p 23032 < "
+            aEjecutar += AUDIOFILE
+            print("Vamos a ejecutar", aEjecutar)
+            os.system(aEjecutar)
 
         if METHOD == 'BYE' :
             LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n"
             print(LINE)
             log("Sent to " + SERVER + ": " + PORT + ":" + LINE, LOGFILE)
-            my_socket.send(bytes(LINE, 'utf-8'))
+            log("Finishing...", LOGFILE)
 
+            LINE = LINE.replace("\r\n", " ")
+            log('Sent to ' + IPPROXY + ':' + str(PORTPROXY) + ': ' +
+                LINE, LOGFILE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            data = my_socket.recv(1024)
+            print('Enviamos al Proxy:\r\n', LINE)
+            LINE = LINE.replace("\r\n", " ")
+            log('Sent to ' + IPPROXY + ':' + str(PORTPROXY) + ': ' +
+                LINE, LOGFILE)
+        if METHOD not in ['REGISTER', 'INVITE', 'ACK', 'BYE']:
+            print('Try REGISTER, INVITE, ACK or BYE')
+            log('405 METHOD NOT ALLOWED. \r\n', LOGFILE)
 
-
-
-
-
-
-
-
-
+            try:
+                data = my_socket.recv(1024)
+            except ConnectionRefusedError:
+                log("Error: No server listening at " + IPPROXY +
+                    " port " + str(PORTPROXY), LOGFILE)
+    log('Finishing.', LOGFILE)
